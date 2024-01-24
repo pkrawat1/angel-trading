8920978613cbf43439d135e88baac0f9a7e7e78b
pkrawat1 <pankajrawat19sept@gmail.com>
2024-01-24 19:35:02 +0530

    reduce historic data

--- lib/angel_trading_web/live/portfolio/portfolio_live.ex
+++ lib/angel_trading_web/live/portfolio/portfolio_live.ex
@@ -205,7 +205,7 @@ defmodule AngelTradingWeb.PortfolioLive do
                symbol_token,
                "FIFTEEN_MINUTE",
                Timex.now("Asia/Kolkata")
-               |> Timex.shift(weeks: if(prev_quote, do: 0, else: -2))
+               |> Timex.shift(weeks: if(prev_quote, do: 0, else: -1))
                |> Timex.shift(hours: if(prev_quote, do: -1, else: 0))
                |> Timex.format!("{YYYY}-{0M}-{0D} {h24}:{0m}"),
                Timex.now("Asia/Kolkata")
