4c01b0445046589612b7a1569c46802372243311
pkrawat1 <pankajrawat19sept@gmail.com>
2024-02-15 16:41:58 +0530

    move client logic to portfolio component

--- lib/angel_trading_web/live/dashboard/portfolio.ex
+++ lib/angel_trading_web/live/dashboard/portfolio.ex
@@ -1,11 +1,15 @@
 defmodule AngelTradingWeb.DashboardLive.Portfolio do
   use AngelTradingWeb, :live_component
 
-  alias AngelTrading.{Account, API}
+  alias AngelTrading.{Account, API, Utils}
 
-  def update(%{client_code: client_code} = assigns, socket) do
+  def update(%{client_code: client_code}, socket) do
+    live_view_pid = self()
+    :ok = Phoenix.PubSub.subscribe(AngelTrading.PubSub, "quote-stream-#{client_code}")
+
     {:ok,
-     assign_async(socket, [:client_code, :client, :holdings, :profile, :funds, :dis_status], fn ->
+     socket
+     |> assign_async(:client, fn ->
        with {:ok, %{body: data}} when is_binary(data) <- Account.get_client(client_code),
             {:ok, %{token: token} = client} <- Utils.decrypt(:client_tokens, data),
             {:profile, {:ok, %{"data" => profile}}} <- {:profile, API.profile(token)},
@@ -14,22 +18,87 @@ defmodule AngelTradingWeb.DashboardLive.Portfolio do
             {:funds, {:ok, %{"data" => funds}}} <- {:funds, API.funds(token)},
             {_, dis_status} <-
               API.verify_dis(token, holdings |> List.first() |> Map.get("isin", "")) do
-         Process.send_after(self(), {:subscribe_to_feed, client_code}, 500)
-         :timer.send_interval(30000, self(), {:subscribe_to_feed, client_code})
+         symbol_tokens = Enum.map(holdings, & &1["symboltoken"])
+
+         Process.send_after(
+           live_view_pid,
+           {:subscribe_to_feed,
+            %{
+              client_code: client_code,
+              symbol_tokens: symbol_tokens,
+              token: client.token,
+              feed_token: client.feed_token
+            }},
+           500
+         )
+
+         :timer.send_interval(
+           30000,
+           live_view_pid,
+           {:subscribe_to_feed,
+            %{
+              client_code: client_code,
+              symbol_tokens: symbol_tokens,
+              token: client.token,
+              feed_token: client.feed_token
+            }}
+         )
+
+         client =
+           Map.merge(client, %{
+             holdings: holdings,
+             profile: profile,
+             funds: funds,
+             dis_status: dis_status
+           })
 
          {:ok,
           %{
-            client_code: client_code,
-            client: client,
-            holdings: holdings,
-            profile: profile,
-            funds: funds,
-            dis_status: dis_status
+            client: Map.merge(client, calculated_overview(client))
           }}
        else
          e ->
            {:error, e}
        end
      end)}
+  end
+
+  def update(%{quote_data: quote_data}, %{assigns: %{client: %{result: %{} = client}}} = socket) do
+    new_ltp = quote_data.last_traded_price
+
+    updated_holding = Enum.find(client.holdings, &(&1["symboltoken"] == quote_data.token))
+
+    if updated_holding && updated_holding["ltp"] != new_ltp do
+      client = %{
+        client
+        | holdings:
+            client.holdings
+            |> Enum.map(fn holding ->
+              if holding["symboltoken"] == quote_data.token do
+                [%{updated_holding | "ltp" => new_ltp}]
+                |> Utils.formatted_holdings()
+                |> List.first()
+              else
+                holding
+              end
+            end)
+      }
+
+      {:ok,
+       assign_async(socket, :client, fn ->
+         {:ok, %{client: Map.merge(client, calculated_overview(client))}}
+       end)}
+    else
+      {:ok, socket}
+    end
+  end
+
+  def update(_, socket), do: {:ok, socket}
+
+  def calculated_overview(client) do
+    client
+    |> Map.get(:holdings)
+    |> Utils.formatted_holdings()
+    |> Utils.calculated_overview()
   end
 end
