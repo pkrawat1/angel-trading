058cd42aeb9102cc988d2e10222e912783237175
pkrawat1 <pankajrawat19sept@gmail.com>
2024-03-07 22:39:33 +0530

    show loader

--- lib/angel_trading_web/live/ask/ask_live.ex
+++ lib/angel_trading_web/live/ask/ask_live.ex
@@ -2,6 +2,7 @@ defmodule AngelTradingWeb.AskLive do
   use AngelTradingWeb, :live_view
   alias AngelTrading.{Account, SmartChat, Utils}
   alias LangChain.Message
+  alias Phoenix.LiveView.AsyncResult
   require Logger
 
   embed_templates "*"
@@ -32,6 +33,7 @@ defmodule AngelTradingWeb.AskLive do
         |> assign(:page_title, "Smart Assistant")
         |> assign(:token, token)
         |> assign(:client_code, client_code)
+        |> assign(:lang_chain, AsyncResult.loading())
         |> start_async(:ask_lang_chain, fn -> SmartChat.new_chain(%{client_token: token}) end)
         |> stream_configure(:messages, dom_id: &"message-#{:erlang.phash2(&1.content)}")
         |> stream(:messages, [])
@@ -48,23 +50,29 @@ defmodule AngelTradingWeb.AskLive do
   def handle_async(:ask_lang_chain, {:ok, {_, lang_chain, response}}, socket) do
     {:noreply,
      socket
-     |> assign(:lang_chain, lang_chain)
+     |> assign(:lang_chain, AsyncResult.ok(socket.assigns.lang_chain, lang_chain))
      |> stream_insert(:messages, response)}
   end
 
   def handle_async(:ask_lang_chain, {:exit, _}, socket) do
-    {:noreply, socket}
+    {:noreply,
+     assign(
+       socket,
+       :lang_chain,
+       AsyncResult.failed(socket.assigns.lang_chain, {:exit, "Seems AI is stuck in traffic."})
+     )}
   end
 
   def handle_event(
         "ask",
         %{"ask" => message},
-        %{assigns: %{lang_chain: lang_chain}} = socket
+        %{assigns: %{lang_chain: %{result: lang_chain}}} = socket
       ) do
     message = Message.new_user!(message)
 
     {:noreply,
      socket
+     |> assign(:lang_chain, AsyncResult.loading())
      |> start_async(:ask_lang_chain, fn ->
        SmartChat.run(lang_chain, [message], [])
      end)
