2accee893477d40c21f33e95a8d5a345d671df4b
pkrawat1 <pankajrawat19sept@gmail.com>
2024-03-17 17:49:59 +0530

    testing with mock

--- test/angel_trading/yahoo_finance_test.exs
+++ test/angel_trading/yahoo_finance_test.exs
@@ -4,19 +4,25 @@ defmodule YahooFinanceTest do
 
   test "search" do
     assert AngelTrading.YahooFinance.search("swan energe") == {:ok, []}
-    assert AngelTrading.YahooFinance.search("swan energy") == {:ok, [
-      %{
-        symbol: "SWANENERGY.NS",
-        __struct__: YQuoteItem,
-        long_name: "Swan Energy Limited"
-      }
-    ]}
-    assert AngelTrading.YahooFinance.search("sbin") == {:ok, [
-      %{
-        symbol: "SBIN.NS",
-        __struct__: YQuoteItem,
-        long_name: "State Bank of India"
-      }
-    ]}
+
+    assert AngelTrading.YahooFinance.search("swan energy") ==
+             {:ok,
+              [
+                %{
+                  symbol: "SWANENERGY.NS",
+                  __struct__: YQuoteItem,
+                  long_name: "Swan Energy Limited"
+                }
+              ]}
+
+    assert AngelTrading.YahooFinance.search("sbin") ==
+             {:ok,
+              [
+                %{
+                  symbol: "SBIN.NS",
+                  __struct__: YQuoteItem,
+                  long_name: "State Bank of India"
+                }
+              ]}
   end
 end
