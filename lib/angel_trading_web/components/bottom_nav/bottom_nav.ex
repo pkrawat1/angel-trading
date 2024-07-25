2e856d7b7d1d7456ecf9457ecfe128a0c7d554b5
pkrawat1 <pankajrawat19sept@gmail.com>
2024-07-25 14:55:00 +0530

    revert ui change

--- lib/angel_trading_web/components/bottom_nav/bottom_nav.ex
+++ lib/angel_trading_web/components/bottom_nav/bottom_nav.ex
@@ -9,7 +9,7 @@ defmodule AngelTradingWeb.Components.BottomNav do
   def bottom_nav(assigns) do
     ~H"""
     <div class="fixed bottom-0 left-0 z-50 w-full py-2 bg-white border-t border-gray-200 dark:bg-gray-900 dark:border-gray-800">
-      <div class="flex h-full justify-between mx-auto px-4 text-xs">
+      <div class="flex h-full justify-center space-x-6 mx-auto text-xs">
         <.link
           navigate={~p"/"}
           type="button"
