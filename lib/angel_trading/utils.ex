ae44a0d96b9c7f87d3b4b9c4767716de62ffb8fe
pkrawat1 <pankajrawat19sept@gmail.com>
2024-12-24 09:23:36 +0530

    remove explorer

--- lib/angel_trading/utils.ex
+++ lib/angel_trading/utils.ex
@@ -1,9 +1,6 @@
 defmodule AngelTrading.Utils do
   @max_age :infinity
 
-  require Explorer.DataFrame, as: DF
-  require Explorer.Series, as: S
-
   @doc """
   Encrypt any Erlang term.
 
@@ -102,25 +99,21 @@ defmodule AngelTrading.Utils do
   end
 
   def formatted_candle_data(candle_data) do
-    candle_data
-    |> Enum.map(&format_candle_data/1)
-    |> calculate_rsi()
-  end
-
-  defp format_candle_data([timestamp, open, high, low, close, volume]) do
-    %{
-      time:
-        timestamp
-        |> String.split("+")
-        |> List.first()
-        |> Timex.parse!("{ISO:Extended:Z}")
-        |> Timex.to_unix(),
-      open: open,
-      high: high,
-      low: low,
-      close: close,
-      volume: volume
-    }
+    Enum.map(candle_data, fn [timestamp, open, high, low, close, volume] ->
+      %{
+        time:
+          timestamp
+          |> String.split("+")
+          |> List.first()
+          |> Timex.parse!("{ISO:Extended:Z}")
+          |> Timex.to_unix(),
+        open: open,
+        high: high,
+        low: low,
+        close: close,
+        volume: volume
+      }
+    end)
   end
 
   def stock_long_name(trading_symbol) do
@@ -130,56 +123,6 @@ defmodule AngelTrading.Utils do
       {:ok, [%{long_name: long_name}]} when byte_size(long_name) > 0 -> long_name
       _ -> trading_symbol
     end
-  end
-
-  defp calculate_rsi(candle_data) do
-    quote_data =
-      candle_data
-      |> DF.new()
-      |> DF.mutate(
-        price_change:
-          S.subtract(
-            close,
-            S.shift(close, 1)
-          )
-      )
-      |> DF.mutate(
-        gain: if(S.greater(price_change, 0), do: price_change, else: 0),
-        loss: if(S.less(price_change, 0), do: -price_change, else: 0)
-      )
-      |> DF.mutate(
-        avg_gain: S.window_mean(gain, 14, min_periods: 1),
-        avg_loss: S.window_mean(loss, 14, min_periods: 1)
-      )
-
-    quote_data_first_13 = DF.slice(quote_data, 0..12)
-
-    quote_data_at_14 =
-      quote_data
-      |> DF.mutate(
-        avg_gain: S.window_mean(gain, 14, min_periods: 14),
-        avg_loss: S.window_mean(loss, 14, min_periods: 14)
-      )
-      |> DF.slice(13..13)
-
-    quote_data_after_14 =
-      quote_data_at_14
-      |> DF.concat_rows(quote_data |> DF.slice(14..-1//1))
-      |> DF.mutate_with(
-        &[
-          avg_gain: S.divide(S.add(&1["gain"], S.multiply(13, S.shift(&1["avg_gain"], 1))), 14),
-          avg_loss: S.divide(S.add(&1["loss"], S.multiply(13, S.shift(&1["avg_loss"], 1))), 14)
-        ]
-      )
-      |> DF.slice(1..-1//1)
-
-    quote_data_first_13
-    |> DF.concat_rows(quote_data_at_14)
-    |> DF.concat_rows(quote_data_after_14)
-    |> DF.mutate(rs: S.divide(avg_gain, avg_loss))
-    |> DF.mutate(rs: if(S.is_nan(rs), do: 0, else: rs))
-    |> DF.mutate(rsi: S.subtract(100, S.divide(100, S.add(1, rs))))
-    |> DF.to_rows()
   end
 
   defp secret(), do: Application.get_env(:angel_trading, :encryption_key)
